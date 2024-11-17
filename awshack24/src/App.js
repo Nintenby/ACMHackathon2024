2d26e8ed1481ea6ab7acaa206b42d8327f00f6de
ramirezvigil24 <137444453+ramirezvigil24@users.noreply.github.com>
2024-11-16 22:23:47 -0800

    Website updates

--- awshack24/src/App.js
+++ awshack24/src/App.js
@@ -1,26 +1,30 @@
 import './App.css';
 
 function App() {
-  
+
   return (
     <div className="App">
       <header className="App-header">
         Emergency Vehicle Alert
       </header>
       <div className="content">
-      <img src = {logo} className = "logoimg" alt = "logo"></img>
-      <img src = {mapimg} className = "mapimg" alt = "map"></img>
         <p className="description"> Never be caught unaware on the road with Emergency
           Vehicle Alert (EVA), the early warning system for when an 
           emergency vehicle is coming your way. Know to move before 
-          you even hear the sirens with an alert to your phone by EVA, 
-          goodbye to the days of last minute swerving and nerves on the 
-          road.
+          you even hear sirens with an alert to your phone by EVA. Swerving and stress
+          are now a thing of the past, rejoice in the future with EVA.
         </p>
-        <button className="button" type="button">Ambulance?</button>
+        <form >
+        <label className="textBox">
+           <input name="url" defaultValue="Insert image url here"/>
+        </label>
+          <button className="button" type="submit" >Ambulance?</button>
+        </form>
       </div>
     </div>
   );
 }
 
 export default App;
+
+
