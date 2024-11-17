8de58fd86d4dc2c0e85e0642a06a25eb7b629c14
izajacquez <izajacquez@gmail.com>
2024-11-16 17:51:31 -0800

    Created site

---
+++ awshack24/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Emergency Vehicle Ale
+      </header>
+    </div>
+  );
+}
+
+export default App;
