d505f3523c4cbbbce2372e5795d9fd28a89e7134
ramirezvigil24 <137444453+ramirezvigil24@users.noreply.github.com>
2024-11-16 18:20:56 -0800

    Website updates to include description

--- awshack24/src/App.js
+++ awshack24/src/App.js
@@ -6,6 +6,15 @@ function App() {
       <header className="App-header">
         Emergency Vehicle Ale
       </header>
+      <div className="content">
+        <p className="description"> Never be caught unaware on the road with Emergency
+          Vehicle Alert (EVA), the early warning system for when an 
+          emergency vehicle is coming your way. Know to move before 
+          you even hear the sirens with an alert to your phone by EVA, 
+          goodbye to the days of last minute swerving and nerves on the 
+          road.
+        </p>
+      </div>
     </div>
   );
 }
