952055bd5e391fa92099d87a877312e0372bdcd0
izajacquez <izajacquez@gmail.com>
2024-11-16 19:12:48 -0800

    img

--- awshack24/src/App.js
+++ awshack24/src/App.js
@@ -7,6 +7,8 @@ function App() {
         Emergency Vehicle Ale
       </header>
       <div className="content">
+      <img src = {logo} className = "logoimg" alt = "logo"></img>
+      <img src = {mapimg} className = "mapimg" alt = "map"></img>
         <p className="description"> Never be caught unaware on the road with Emergency
           Vehicle Alert (EVA), the early warning system for when an 
           emergency vehicle is coming your way. Know to move before 
