983ec4a377f596ad82dd7cc1f93d708eba73bc08
ramirezvigil24 <137444453+ramirezvigil24@users.noreply.github.com>
2024-11-16 23:35:21 -0800

    API'd website

--- awshack24/src/App.js
+++ awshack24/src/App.js
@@ -1,6 +1,31 @@
 import './App.css';
-
+import React, { useState} from 'react';
+ 
 function App() {
+  const [result, setResult] = useState("");
+
+  function handleSubmit(e) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+    console.log("hello please log");
+  
+    // Read the form data
+    const form = e.target;
+    const urly = new FormData(form).get("url");
+  
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow"
+    };
+  
+    fetch(`http://172.31.30.58:3001/?url=${urly}`, requestOptions) 
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setResult(data); // Update the result state correctly
+    })
+    .catch((error) => console.error(error));
+  };
 
   return (
     <div className="App">
@@ -14,12 +39,13 @@ function App() {
           you even hear sirens with an alert to your phone by EVA. Swerving and stress
           are now a thing of the past, rejoice in the future with EVA.
         </p>
-        <form >
+        <form method="post" onSubmit={handleSubmit}>
         <label className="textBox">
            <input name="url" defaultValue="Insert image url here"/>
         </label>
           <button className="button" type="submit" >Ambulance?</button>
         </form>
+        <p className="resultTime">{result}</p>
       </div>
     </div>
   );
